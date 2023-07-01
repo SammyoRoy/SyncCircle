7c3e2a4125586a79fc0e2c9ccfbcbc9fec366698
SammyoRoy <sammyo.roychowdhury@gmail.com>
2023-07-01 14:03:01 -0700

    Added static Days of the Week header bar

--- synccircle/src/components/UserPage/UserPage.jsx
+++ synccircle/src/components/UserPage/UserPage.jsx
@@ -57,19 +57,43 @@ function JoinButton(){
 }
 
 function DaysOfTheWeek(){
+  return (
+    <div className="DOTWBar">
+      <ScrollIcon />
+      <DayLabels />
+      <DayLabels />
+      <DayLabels />
+      <DayLabels />
+      <DayLabels />
+      <DayLabels />
+      <DayLabels />
+    </div>
+  )
+}
 
+function ScrollIcon(){
+  return (
+    <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <path d="M17 15L12 20L7 15" stroke="#6ABEA7" stroke-width="2"/>
+      <path d="M17 9L12 4L7 9" stroke="#6ABEA7" stroke-width="2"/>
+    </svg>
+  )
 }
 
 function DayLabels(){
-
+  return (
+    <div className="DayLabel"> S</div>
+  )
 }
 
 function HeaderCard(){
 
   return (
-      <div className= "HeaderCard">
-        <UserNameForm />
-        
+      <div>
+        <div className= "HeaderCard">
+          <UserNameForm />
+        </div>
+        <DaysOfTheWeek />
       </div>
   )
 
