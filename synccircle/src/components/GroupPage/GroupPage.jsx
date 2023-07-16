b9ef9b9b35641e2686f7830b7fb558468875e063
SammyoRoy <sammyo.roychowdhury@gmail.com>
2023-07-16 13:51:53 -0700

    Group page slots dynamically colored

--- synccircle/src/components/GroupPage/GroupPage.jsx
+++ synccircle/src/components/GroupPage/GroupPage.jsx
@@ -129,7 +129,7 @@ return (
 }
 
 function Slot({ matrixKey, days, groupId, userId }){
-const [color, setColor] = useState("F7F7F7");
+const [color, setColor] = useState("#F7F7F7");
 const [numAvail, setNumAvail] = useState(0);
 const [totalMembers, setTotalMembers] = useState(0)
 const cols = days.length;
@@ -137,67 +137,57 @@ const cols = days.length;
 const row = Math.floor(matrixKey/(cols+1));
 const col = matrixKey - (row *(cols+1)) - 1;
 
+useEffect(() => {
+  async function fetchData() {
+    try {
+      const response1 = await axios.post(
+        `http://localhost:4000/slot?group=${groupId}=${row}=${col}`
+      );
+      setNumAvail(parseInt(response1.data));
 
-  useEffect(() => {
-    
-    async function initialize(){
-      await axios.post(`http://localhost:4000/slot?group=${groupId}=${row}=${col}`)
-      .then((response) => {
-          // navigate to /group pages
-          setNumAvail(parseInt(response.data));
-      })
-      .catch((error) => {
-          // handle the error
-          console.error(error);
-      });
+      const response2 = await axios.post(
+        `http://localhost:4000/numMem?group=${groupId}`
+      );
+      setTotalMembers(parseInt(response2.data));
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  fetchData();
+}, [groupId, row, col]);
+
   
-      await axios.post(`http://localhost:4000/numMem?group=${groupId}`)
-        .then((response) => {
-          setTotalMembers(parseInt(response.data));
-        })
-    }
+useEffect(() => {
+  setColorByRatio();
+}, [numAvail, totalMembers]);
 
+function setColorByRatio() {
+  const ratio = numAvail / totalMembers;
+  console.log(ratio);
 
-    function setColor() {
-      const ratio = numAvail/totalMembers;
-      console.log("Hello");
-      
-      if (ratio == 1){
-        setColor("058ED9");
-      }
-      else if (ratio >= .9){
-        setColor("17881C");
-      }
-      else if (ratio >= .8){
-        setColor("3FB444");
-      }
-      else if (ratio >= .7){
-        setColor("81DE84");
-      }
-      else if (ratio >= .6){
-        setColor("A39E3D");
-      }
-      else if (ratio >= .5){
-        setColor("D2CD67");
-      }
-
-      else if (ratio >= .4){
-        setColor("E5E296");
-      }
-      else if (ratio >= .3){
-        setColor("984A45");
-      }
-      else if (ratio >= .2){
-        setColor("C65E58");
-      }
-      else if (ratio >= .1){
-        setColor("F4A19C");
-      }
-    }
-    initialize();
-    setColor();
-  }, [numAvail, totalMembers, color]);
-
+  if (ratio == 1) {
+    setColor(`#058ED9`);
+  } else if (ratio >= 0.9) {
+    setColor(`#17881C`);
+  } else if (ratio >= 0.8) {
+    setColor(`#3FB444`);
+  } else if (ratio >= 0.7) {
+    setColor(`#81DE84`);
+  } else if (ratio >= 0.6) {
+    setColor(`#A39E3D`);
+  } else if (ratio >= 0.5) {
+    setColor(`#D2CD67`);
+  } else if (ratio >= 0.4) {
+    setColor(`#E5E296`);
+  } else if (ratio >= 0.3) {
+    setColor(`#984A45`);
+  } else if (ratio >= 0.2) {
+    setColor(`#C65E58`);
+  } else if (ratio >= 0.1) {
+    setColor(`#F4A19C`);
+  }
+}
 
 
 return (
