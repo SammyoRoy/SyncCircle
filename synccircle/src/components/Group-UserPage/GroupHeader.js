06632dffab398ff2e318c9797a5a1bcdba4e1e9d
SammyoRoy <sammyo.roychowdhury@gmail.com>
2023-07-24 15:38:07 -0700

    Group Page styling

--- synccircle/src/components/Group-UserPage/GroupHeader.js
+++ synccircle/src/components/Group-UserPage/GroupHeader.js
@@ -1,12 +1,14 @@
 import React from 'react';
+import ShareLink from './ShareLink';
 import DaysOfTheWeek from './DaysOfTheWeek';
 
 
 function GroupHeader(){
 
     return (
-        <div>
-          <DaysOfTheWeek />
+        <div className="GroupHeaderCard">
+          <ShareLink />
+          <DaysOfTheWeek styling={"GroupDOTWBar"} />
         </div>
     )
   
--- synccircle/src/components/Group-UserPage/GroupPage.jsx
+++ synccircle/src/components/Group-UserPage/GroupPage.jsx
@@ -4,18 +4,11 @@ import UserTitle from "./UserTitle";
 import GroupCalendar from "./GroupCalender";
 import GroupHeader from "./GroupHeader";
 import Copy from "./copy.png";
+import GroupTitle from "./GroupTitle"
+import './GroupPageStyle.css';
 
 function GroupPage() {
   const { groupId, userId } = useContext(AppContext);
-  const [isCopied, setIsCopied] = useState(false);
-
-  const handleCopyClick = () => {
-    navigator.clipboard.writeText(window.location.href);
-    setIsCopied(true);
-    setTimeout(() => {
-      setIsCopied(false);
-    }, 2000); // 2000 milliseconds (2 seconds) delay before hiding the "copied" text
-  };
 
   return (
     <div className="LightMode">
@@ -32,19 +25,9 @@ function GroupPage() {
           aria-label="Close"
         ></button>
         <div className="offcanvas-header">
-          <UserTitle />
+          <GroupTitle />
         </div>
         <div className="offcanvas-body">
-          <div className="share-link">
-            <h5>Invite Your Friends</h5>
-            <h6>
-              Copy Link to Clipboard:{" "}
-              <button className="btn" onClick={handleCopyClick}>
-                <img src={Copy} />
-              </button>{" "}
-              {isCopied && <span style={{ color: "green" }}>Copied</span>}
-            </h6>
-          </div>
           <GroupHeader />
           <GroupCalendar />
         </div>
