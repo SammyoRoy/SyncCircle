a18aaecd3a5b12b5d70c04bba1ce166f08b96a4d
NithinT7 <90013428+NithinT7@users.noreply.github.com>
2023-08-15 12:18:16 +0530

    GroupCalander shows up on max array

--- synccircle/src/components/Group-UserPage/GroupCalender.js
+++ synccircle/src/components/Group-UserPage/GroupCalender.js
@@ -85,7 +85,17 @@ function GroupCalendar({ setPopupMatrixKey, setPopupColor }) {
         parsedHour = 0;
       }
     
-      return (parsedHour - 6);
+      const timeOptions = [
+        '6:00 AM', '7:00 AM', '8:00 AM', '9:00 AM', '10:00 AM', '11:00 AM',
+        '12:00 PM', '1:00 PM', '2:00 PM', '3:00 PM', '4:00 PM', '5:00 PM',
+        '6:00 PM', '7:00 PM', '8:00 PM', '9:00 PM', '10:00 PM', '11:00 PM',
+        '12:00 AM', '1:00 AM', '2:00 AM', '3:00 AM', '4:00 AM', '5:00 AM'
+      ];
+    
+      // Find the index of the parsed time in the timeOptions array
+      const index = timeOptions.findIndex(option => option === time);
+    
+      return index;
     }
     
     const startTimeIndex = convertTimeToIndex(start);
@@ -123,6 +133,9 @@ function GroupCalendar({ setPopupMatrixKey, setPopupColor }) {
   }, [groupSocket]); // Add groupSocket as a dependency
 
   function getNumAvail(row, col){
+    if (numAvailArr === null){
+      return;
+    }
     const num = numAvailArr[row][col];
     return num;
   }
