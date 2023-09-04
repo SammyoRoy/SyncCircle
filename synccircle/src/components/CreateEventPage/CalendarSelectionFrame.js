3fd5bc83085a74a8dfd5045be20b71915d10de6e
NithinT7 <90013428+NithinT7@users.noreply.github.com>
2023-09-04 16:13:41 +0530

    Range array working for Calendar
    Range array works, but several bugs mentioned on discord

--- synccircle/src/components/CreateEventPage/CalendarSelectionFrame.js
+++ synccircle/src/components/CreateEventPage/CalendarSelectionFrame.js
@@ -1,28 +1,53 @@
-import React, {useState} from 'react'
-import Calender from "react-calendar"
+import React, { useState } from 'react';
+import Calendar from 'react-calendar';
 import 'react-calendar/dist/Calendar.css';
+import { Button } from '@mui/material';
 
 const CalendarSelectionFrame = () => {
-    const [date, setDate] = useState([]);
+  const [dateRanges, setDateRanges] = useState([]);
 
-    const onChange = date => {
-        setDate(date);
+  const onChange = (date) => {
+    if (dateRanges.includes(date)) {
+      setDateRanges(dateRanges.filter(range => range !== date));
+      return;
     }
-    console.log(date);
+    else{
+      setDateRanges([...dateRanges, date]);
+    }
+  };
+
+  const onClear = () => {
+    setDateRanges([]);
+  };
+
+  const isDateInRanges = (date) => {
+    return dateRanges.flat().some(range => 
+      date >= new Date(range[0]) && date <= new Date(range[1])
+    );
+  };
+
+  const tileContent = ({ date, view }) => {
+    return view === 'month' && isDateInRanges(date) ? <div className='calendar-range' /> : null;
+  };
+
+  console.log(dateRanges);
+
   return (
     <div className="DaySelectionFrame">
-        <Calender 
-          minDetail={"month"}
-          next2Label={null}
-          prev2Label={null}
-          onChange={onChange} 
-          value={date} 
-          selectRange={true} 
-          minDate={new Date()}
-          
-          tileClassName={"react-calendar-button"}/>
+      <Calendar
+        minDetail={"month"}
+        next2Label={null}
+        prev2Label={null}
+        onChange={onChange}
+        value={dateRanges.flat()} 
+        selectRange={true}
+        minDate={new Date()}
+        tileClassName={"react-calendar-button"}
+        tileContent={tileContent}
+      />
+      <Button onClick={onClear}>Clear</Button>
     </div>
-  )
-}
+  );
+};
 
-export default CalendarSelectionFrame
+export default CalendarSelectionFrame;
