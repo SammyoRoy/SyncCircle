fdcba15f408e9b1f8ed11d16067e16a97b2a8d20
NithinT7 <90013428+NithinT7@users.noreply.github.com>
2023-09-07 21:35:45 +0530

    CSS styling for new Calendar

--- synccircle/src/components/CreateEventPage/CalendarSelectionFrame.js
+++ synccircle/src/components/CreateEventPage/CalendarSelectionFrame.js
@@ -3,37 +3,15 @@ import { Button } from '@mui/material';
 import { Calendar } from "react-multi-date-picker"
 
 const CalendarSelectionFrame = () => {
-  /*const [dateRanges, setDateRanges] = useState([]);
-
-  const onChange = (date) => {
-    if (dateRanges.includes(date)) {
-      setDateRanges(dateRanges.filter(range => range !== date));
-      return;
-    }
-    else{
-      setDateRanges([...dateRanges, date]);
-    }
-  };
-
-  const onClear = () => {
-    setDateRanges([]);
-  };
-
-  const isDateInRanges = (date) => {
-    return dateRanges.flat().some(range => 
-      date >= new Date(range[0]) && date <= new Date(range[1])
-    );
-  };
-
-  const tileContent = ({ date, view }) => {
-    return view === 'month' && isDateInRanges(date) ? <div className='calendar-range' /> : null;
-  };
-
-  console.log(dateRanges); */
+  
 
   return (
-    <div className="DaySelectionFrame">
+    <div className="CalendarSelectionFrame">
       <Calendar
+        monthYearSeparator="|"
+        showOtherDays={true}
+        disableYearPicker={true}
+        minDate={new Date()}
         range
         multiple
       />
