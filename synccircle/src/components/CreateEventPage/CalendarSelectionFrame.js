bbcf939812912db11e7660dbeabbaceba5e7584c
NithinT7 <90013428+NithinT7@users.noreply.github.com>
2023-09-19 18:19:10 +0530

    Delete dates function

--- synccircle/src/components/CreateEventPage/CalendarSelectionFrame.js
+++ synccircle/src/components/CreateEventPage/CalendarSelectionFrame.js
@@ -1,31 +1,35 @@
-import React, { useContext, useState } from 'react';
+import React, { useState } from 'react';
 import { Calendar } from 'react-multi-date-picker';
-import { AppContext } from '../../context/AppContext';
+import { Button } from '@mui/material';
 
-const CalendarSelectionFrame = ({days, setDays}) => {
+const CalendarSelectionFrame = ({ days, setDays }) => {
+  const [selectedDates, setSelectedDates] = useState(days || []);
 
   const handleChange = (value) => {
     const flatDates = flattenAndFill(value);
     const sortedDates = flatDates.sort((a, b) => a - b);
     const formattedDates = sortedDates.map(date => formatDateString(date));
     setDays(formattedDates);
-    console.log(formattedDates);
+    setSelectedDates(value);
   };
-  
+
+  const deleteAllDates = () => {
+    setSelectedDates([]);
+    setDays([]);
+  };
+
   function flattenAndFill(dates) {
     let flatDates = [];
     dates.forEach(element => {
       if (Array.isArray(element)) {
-        // For a range of dates
         let startDate = new Date(element[0].year, element[0].month.number - 1, element[0].day);
         let endDate = new Date(element[element.length - 1].year, element[element.length - 1].month.number - 1, element[element.length - 1].day);
-        
+
         while (startDate <= endDate) {
           flatDates.push(new Date(startDate));
           startDate.setDate(startDate.getDate() + 1);
         }
       } else {
-        // For a single date
         flatDates.push(new Date(element.year, element.month.number - 1, element.day));
       }
     });
@@ -39,14 +43,11 @@ const CalendarSelectionFrame = ({days, setDays}) => {
     const weekday = weekDayNames[date.getDay()];
     return [monthDay, weekday];
   }
-  
-  
-  
-  
 
   return (
     <div className="CalendarSelectionFrame">
       <Calendar
+        value={selectedDates}
         monthYearSeparator="|"
         showOtherDays={true}
         disableYearPicker={true}
@@ -55,6 +56,7 @@ const CalendarSelectionFrame = ({days, setDays}) => {
         multiple
         onChange={handleChange}
       />
+      <Button onClick={deleteAllDates}>Delete All Dates</Button>
     </div>
   );
 };
