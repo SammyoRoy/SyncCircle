8fdd8df18aac3aeba42c3061f07fb10603d851db
NithinT7 <90013428+NithinT7@users.noreply.github.com>
2023-12-28 22:05:57 +0530

    Initial Commit
    route to landing Page -> /temp

--- synccircle/src/App.jsx
+++ synccircle/src/App.jsx
@@ -7,6 +7,7 @@ import {
 import CreateEventPage from './components/CreateEventPage/CreateEventPage.jsx';
 import UserPage from './components/Group-UserPage/UserPage.jsx';
 import GroupPage from './components/Group-UserPage/GroupPage.jsx';
+import { LandingPage } from './components/LandingPage/LandingPage.jsx';
 
 function App() {
     return (
@@ -15,6 +16,7 @@ function App() {
           <Routes>
             <Route path="/" element={<CreateEventPage />} />
             <Route path="/group/:group" element={<UserPage/>} />
+            <Route path="/temp" element={<LandingPage/>} />
           </Routes>
         </div>
       </Router>
