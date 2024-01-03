085587af2fcebf9d7a968ba45326504c658ac22e
NithinT7 <90013428+NithinT7@users.noreply.github.com>
2024-01-03 15:14:15 +0530

    Set Up Tutorial Grids w/o images

--- synccircle/src/components/LandingPage/Tutorial.js
+++ synccircle/src/components/LandingPage/Tutorial.js
@@ -6,12 +6,41 @@ const Tutorial = () => {
             <h1>Sync your schedules with one easy link</h1>
             <div className='Steps'>
                 <div className='Step1'>
-
+                    <div className='Count'>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="33" height="33" viewBox="0 0 33 33" fill="none">
+                            <circle cx="16.5" cy="16.5" r="16" fill="#297045" />
+                            <text className='Text' fill='#FFF' x="50%" y="50%" textAnchor="middle" dy=".3em">1</text>
+                        </svg>
+                    </div>
+                    <h3>Create an event</h3>
+                    <b3>
+                        Enter an event name, time range, and select days
+                    </b3>
                 </div>
                 <div className='Step2'>
+                    <div className='Count'>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="33" height="33" viewBox="0 0 33 33" fill="none">
+                            <circle cx="16.5" cy="16.5" r="16" fill="#297045" />
+                            <text className='Text' fill='#FFF' x="50%" y="50%" textAnchor="middle" dy=".3em">2</text>
+                        </svg>
+                    </div>
+                    <h3>Fill out your availability</h3>
+                    <b3>
+                        Type in a username, and then press the time slots for when you’re free. If you need to update your availability later, you can log back in with the same username.
+                    </b3>
 
                 </div>
                 <div className='Step3'>
+                    <div className='Count'>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="33" height="33" viewBox="0 0 33 33" fill="none">
+                            <circle cx="16.5" cy="16.5" r="16" fill="#297045" />
+                            <text className='Text' fill='#FFF' x="50%" y="50%" textAnchor="middle" dy=".3em">3</text>
+                        </svg>
+                    </div>
+                    <h3>Share the link!</h3>
+                    <b3>
+                        Use purple group button to open the group tab. Copy the link and send it to your friends. Then watch as the slots update in real time as they fill in their availabilities.
+                    </b3>
 
                 </div>
             </div>
