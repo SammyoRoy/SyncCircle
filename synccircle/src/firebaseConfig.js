3e6de90daaac1b19f93952c0d89790dfc847c487
NithinT7 <90013428+NithinT7@users.noreply.github.com>
2024-01-08 14:17:19 -0800

    Firebase analytics

--- synccircle/src/firebaseConfig.js
+++ synccircle/src/firebaseConfig.js
@@ -1,6 +1,8 @@
 // Import the functions you need from the SDKs you need
+import { useEffect } from "react";
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
+import { getAnalytics, logEvent } from "firebase/analytics";
+import { useLocation } from "react-router";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -19,3 +21,14 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
+
+const usePageTracking = () => {
+  let location = useLocation();
+  useEffect(() => {
+    logEvent(analytics,'page_view', {
+      page_path: location.pathname,
+    });
+  }, [location]);
+}
+
+export default usePageTracking;
